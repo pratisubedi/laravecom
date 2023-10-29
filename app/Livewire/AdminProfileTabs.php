2ff11c59e71554b198090b72f670dd3ed4401ee1
pratiksubedi <pratiksubedi98998@gmail.com>
2023-10-29 17:08:39 +0545

    good livewire

--- app/Livewire/AdminProfileTabs.php
+++ app/Livewire/AdminProfileTabs.php
@@ -3,9 +3,19 @@
 namespace App\Livewire;
 
 use Livewire\Component;
-
+use Illuminate\Support\Facades\Auth;
+use App\Models\Admin;
 class AdminProfileTabs extends Component
 {
+    public $tab=null;
+    public $tabname='personal_details';
+    protected $queryString= ['tab'];
+    public function selectTab($tab){
+        $this->tab=$tab;
+    }
+    public function mount(){
+        $this->tab=request()->tab ? request()->tab(): $this->tabname;
+    }
     public function render()
     {
         return view('livewire.admin-profile-tabs');
