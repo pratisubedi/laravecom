2e356d7800af4743ec7ebf06fb801b3680b46797
pratiksubedi <pratiksubedi98998@gmail.com>
2023-11-01 17:03:03 +0545

    make livewire workable

--- app/Livewire/AdminSetting.php
+++ app/Livewire/AdminSetting.php
@@ -6,6 +6,16 @@ use Livewire\Component;
 
 class AdminSetting extends Component
 {
+    public $tab=null;
+    public $default_tab='general_settings';
+    protected  $queryString=['tab'];
+    public function selectTab($tab){
+        $this->tab=$tab;
+    }
+
+    public function mount(){
+        $this->tab=request()->tab ? request()->tab : $this->default_tab;
+    }
     public function render()
     {
         return view('livewire.admin-setting');
--- resources/views/livewire/admin-setting.blade.php
+++ resources/views/livewire/admin-setting.blade.php
@@ -2,35 +2,35 @@
     <div class="tab">
         <ul class="nav nav-tabs customtab" role="tablist">
             <li class="nav-item">
-                <a class="nav-link active" data-toggle="tab" href="#general_settings" role="tab" aria-selected="true">General Settings</a>
+                <a wire:click.prevent='selectTab("general_settings")' class="nav-link active {{$tab=='general_settings' ? 'active':''}}" data-toggle="tab" href="#general_settings" role="tab" aria-selected="true">General Settings</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" data-toggle="tab" href="#logo_favicon" role="tab" aria-selected="false">Logo & Favicon</a>
+                <a wire:click.prevent='selectTab("logo_favicon")' class="nav-link {{$tab=='logo_favicon' ? 'active':''}}" data-toggle="tab" href="#logo_favicon" role="tab" aria-selected="false">Logo & Favicon</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" data-toggle="tab" href="#social_networks" role="tab" aria-selected="false">Social Networks</a>
+                <a wire:click.prevent='selectTab("social_networks")' class="nav-link {{$tab=='social_networks' ? 'active':''}}" data-toggle="tab" href="#social_networks" role="tab" aria-selected="false">Social Networks</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" data-toggle="tab" href="#payment_methods" role="tab" aria-selected="false">Payment Methods</a>
+                <a wire:click.prevent='selectTab("payment_methods")' class="nav-link {{$tab=='payment_methods'  ? 'active':''}}" data-toggle="tab" href="#payment_methods" role="tab" aria-selected="false">Payment Methods</a>
             </li>
         </ul>
         <div class="tab-content">
-            <div class="tab-pane fade show active" id="general_settings" role="tabpanel">
+            <div class="tab-pane fade {{$tab=='general_settings' ? 'active show' :''}}" id="general_settings" role="tabpanel">
                 <div class="pd-20">
                     -------General Setting----
                 </div>
             </div>
-            <div class="tab-pane fade" id="logo_favicon" role="tabpanel">
+            <div class="tab-pane fade {{$tab=='logo_favicon' ? 'active show' :''}}" id="logo_favicon" role="tabpanel">
                 <div class="pd-20">
                     -----Logo & Favicon--
                 </div>
             </div>
-            <div class="tab-pane fade" id="social_networks" role="tabpanel">
+            <div class="tab-pane fade {{$tab=='social_networks' ? 'active show' :''}}" id="social_networks" role="tabpanel">
                 <div class="pd-20">
                     -----Social Networks-----
                 </div>
             </div>
-            <div class="tab-pane fade" id="payment_methods" role="tabpanel">
+            <div class="tab-pane fade {{$tab=='payment_methods' ? 'active show' :''}}" id="payment_methods" role="tabpanel">
                 <div class="pd-20">
                     -----Payment Methods-----
                 </div>
