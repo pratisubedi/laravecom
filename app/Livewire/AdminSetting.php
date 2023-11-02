b28adcb5e85d32d819b2ac41a6913d044f17e0d6
pratiksubedi <pratiksubedi98998@gmail.com>
2023-11-02 13:14:50 +0545

    logo s etting

--- app/Livewire/AdminSetting.php
+++ app/Livewire/AdminSetting.php
@@ -10,7 +10,7 @@ class AdminSetting extends Component
     public $tab=null;
     public $default_tab='general_settings';
     protected  $queryString=['tab'];
-    public $site_name,$site_email,$ite_phone ,$site_meta_keywords,$site_meta_description;
+    public $site_name,$site_email,$site_phone ,$site_meta_keywords,$site_meta_description,$site_logo,$site_favicon;
     public function selectTab($tab){
         $this->tab=$tab;
     }
@@ -22,7 +22,8 @@ class AdminSetting extends Component
         $this->site_phone=get_settings()->site_phone;
         $this->site_meta_keywords=get_settings()->site_meta_keywords;
         $this->site_meta_description=get_settings()->site_meta_description;
-        
+        $this->sit_logo=get_settings()->sit_logo;
+        $this->site_favicon=get_settings()->site_favicon;
     }
     public function updateGeneralSettings(){
         $this->validate([
@@ -33,6 +34,7 @@ class AdminSetting extends Component
         $settings=$settings->first();
         $settings->site_name=$this->site_name;
         $settings->site_email=$this->site_email;
+        $settings->site_phone=$this->site_phone;
         $settings->site_meta_keywords=$this->site_meta_keywords;
         $settings->site_meta_description=$this->site_meta_description;
         $update=$settings->update();
