4fa9353f3b519416d5fa71d924ed6aa9b785f031
pratiksubedi <pratiksubedi98998@gmail.com>
2023-11-02 11:09:21 +0545

    use proected fillable for general settings

--- app/Models/GeneralSetting.php
+++ app/Models/GeneralSetting.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class GeneralSetting extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        "site_name",
+        "site_email",
+        "site_phone",
+        "site_meta_keywords",
+        "site_meta_description",
+    ] ;
 }
