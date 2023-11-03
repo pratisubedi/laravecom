079a4b7becc073609a5b5ae3bc4e13afeaa69018
pratiksubedi <pratiksubedi98998@gmail.com>
2023-11-03 15:27:15 +0545

    databse migration

--- app/Models/SocialNetwork.php
+++ app/Models/SocialNetwork.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class SocialNetwork extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        "facebook_url",
+        "youtube_url",
+        "instagram_url",
+        "linkedin_url",
+        "twitter_url",
+        "github_url"
+    ] ;
 }
